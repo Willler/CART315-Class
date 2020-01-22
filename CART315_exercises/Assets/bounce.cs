87b37d47fde4811b5c6cd8f5ef8316b0a4d86b38
William L'Eriger <espermentalout@gmail.com>
2020-01-22 12:16:45 -0500

    Class 3
    end of class

---
+++ CART315_exercises/Assets/bounce.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounce : MonoBehaviour
+{
+
+    public GameObject the_other_object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        other.GetComponent<Rigidbody>().AddForce(0, 800.0f*Time.deltaTime, 0);
+        if (other.gameObject == the_other_object)
+        {
+            other.GetComponent<Rigidbody>().AddForce(0, 800.0f * Time.deltaTime, 0);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == the_other_object) {
+            other.GetComponent<AudioSource>().Play();
+        }
+        
+    }
+}
--- CART315_exercises/Assets/rotate.cs
+++ CART315_exercises/Assets/rotate.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour
 {
+
+    public float rotate_speed = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class rotate : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.Rotate(0.0f,1.5f,0.0f);
+        if (Input.GetButton("Fire1"))
+        {
+            this.transform.Rotate(0.0f, rotate_speed, 0.0f);
+        }
+        
     }
 }
