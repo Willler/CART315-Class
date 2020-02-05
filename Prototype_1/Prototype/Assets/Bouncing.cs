75a6e504e553278d68d452dbfd5c157777f39162
William L'Eriger <espermentalout@gmail.com>
2020-02-05 02:46:48 -0500

    destroyable objects

---
+++ Prototype_1/Prototype/Assets/Bouncing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncing : MonoBehaviour {
+    public GameObject PlayerCtrl;
+
+
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider PlayerCtrl)
+    {
+
+        PlayerCtrl.GetComponent<Rigidbody>().AddForce(0, 50000.0f * Time.deltaTime, 0);
+
+    }
+}
---
+++ Prototype_1/Prototype/Assets/DestroyTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTarget : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+    }
+}
