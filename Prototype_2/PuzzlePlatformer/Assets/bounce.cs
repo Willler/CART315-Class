a49d0b812fc400c7baa4747a2cc1864bfa3e8c6f
William L'Eriger <espermentalout@gmail.com>
2020-02-12 02:48:12 -0500

    p2: added more + Goal

---
+++ Prototype_2/PuzzlePlatformer/Assets/bounce.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounce : MonoBehaviour
+{
+
+    public AudioClip bouncing;
+
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = bouncing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
