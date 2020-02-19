5999ed03ea7cf4abdf74c9ddbf35375c58311a33
William L'Eriger <espermentalout@gmail.com>
2020-02-19 01:00:34 -0500

    worked on pretty much everything

---
+++ Prototype_3/Prototype3/Assets/TimerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+
+{
+
+    public Text TimerText;
+    public float startTime;
+    public float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t = Time.time - startTime;
+        string seconds = t.ToString("f0");
+        TimerText.text = seconds;
+
+    }
+}
